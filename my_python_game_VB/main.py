22c01aa9b8c1f0bb4923ae00fbb9c60a25ba1216
kay-learn-coding <Lekanfornow@gmail.com>
2026-07-08 20:35:03 +0200

    changes

---
+++ my_python_game_VB/main.py
@@ -0,0 +1,15 @@
+import time
+import random
+import sys
+import tkinter as tk #extreme_lite(info)
+from tkinter import Label
+from tkinter import Button
+from tkinter import Entry
+from PIL import Image, ImageTk
+import os
+import chapter_1
+
+def main():
+    chapter_1()
+    
+
